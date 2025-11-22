2cacb5a9fb31a7526d0eb0518427ec43dfb03004
Manjeet2001 <manjeetydv16@gmail.com>
2025-11-22 12:08:51 +0530

    incorporated basic changes

--- webhook_delivery/src/main/java/com/example/webhook_delivery/repository/SubscriptionRepository.java
+++ webhook_delivery/src/main/java/com/example/webhook_delivery/repository/SubscriptionRepository.java
@@ -2,10 +2,12 @@ package com.example.webhook_delivery.repository;
 
 import com.example.webhook_delivery.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
     List<Subscription> findByEventType(String eventType);
 }
--- webhook_delivery/src/main/java/com/example/webhook_delivery/repository/WebhookDeliveryLogRepository.java
+++ webhook_delivery/src/main/java/com/example/webhook_delivery/repository/WebhookDeliveryLogRepository.java
@@ -3,10 +3,13 @@ package com.example.webhook_delivery.repository;
 import com.example.webhook_delivery.entity.WebhookDeliveryLog;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface WebhookDeliveryLogRepository extends JpaRepository<WebhookDeliveryLog, Long> {
     List<WebhookDeliveryLog> findByDeliveryTaskIdOrderByTimestampAsc(UUID deliveryTaskId);
     List<WebhookDeliveryLog> findBySubscriptionIdOrderByTimestampDesc(UUID subscriptionId, Pageable pageable);
